2eecaa6041b606b47e778f847152619751614071
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-19 02:12:18 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,11 @@
-$RG = "nexusrg"
+$RG = "testinart"
 $location = "centralus"
 New-AzureRmResourceGroup -Name $RG -Location $location
-New-AzureRmResourceGroupDeployment -Name nexus -ResourceGroupName $RG `
+New-AzureRmResourceGroupDeployment -Name jenkinsaspaas -ResourceGroupName $RG `
  -TemplateUri "https://raw.githubusercontent.com/sangaml/artifactory_DSC/master/infra.json" `
 -TemplateParameterUri "https://raw.githubusercontent.com/sangaml/artifactory_DSC/master/infra.parameters.json"
+
+$ipname = (Get-AzureRmResource  -ResourceGroupName $RG  -ResourceType Microsoft.Network/publicIPAddresses).Name[1]
+$IP = (Get-AzureRmPublicIpAddress -Name $ipname -ResourceGroupName $RG).IpAddress
+Write-Host "Login from browser with $IP and port 8080" -ForegroundColor Green 
+Write-Host "Login Username is admin and Password is admin123" -ForegroundColor Green
