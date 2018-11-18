714c51756336ba438c91121ab1eae0c16b3880de
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-18 18:40:47 +0530

    Create nartifactory.ps1

---
+++ nartifactory.ps1
@@ -0,0 +1,10 @@
+Start-BitsTransfer -Source "https://csgdfe49495dc73x47efxabf.blob.core.windows.net/grt/artifactory-oss-6.5.2.zip' -OutFile 'D:\artifactory-oss-6.5.2.zip" `
+-Destination "D:\"
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  function unzip {
+   param( [string]$ziparchive, [string]$extractpath )
+   [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
+    }
+  unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
+
+  D:\artifactory-oss-6.5.2\bin\run.vbs
