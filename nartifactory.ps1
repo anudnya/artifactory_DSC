02cf6b928eeaebf615fdeea8ac7ff4a3052c6a10
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-19 00:46:54 +0530

    Update nartifactory.ps1

--- nartifactory.ps1
+++ nartifactory.ps1
@@ -1,8 +1,9 @@
-Start-BitsTransfer -Source "https://csgdfe49495dc73x47efxabf.blob.core.windows.net/grt/artifactory-oss-6.5.2.zip" -Destination "D:\"
-               Add-Type -AssemblyName System.IO.Compression.FileSystem
-               function unzip {
-               param( [string]$ziparchive, [string]$extractpath )
-               [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
-                }
-               unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
-               D:\artifactory-oss-6.5.2\bin\run.vbs
+Start-BitsTransfer -Source "https://sonatype-download.global.ssl.fastly.net/repository/repositoryManager/3/nexus-3.14.0-04-win64.zip" -Destination "D:\"
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  function unzip {
+   param( [string]$ziparchive, [string]$extractpath )
+   [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
+    }
+  unzip "D:\nexus-3.14.0-04-win64.zip" "D:\"
+
+  D:\nexus-3.14.0-04\bin\nexus.exe start
